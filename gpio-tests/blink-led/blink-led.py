7c1130f550cb1df37bc10f7b59a985612f54eabb
DrakeProvost <mddmprovost@gmail.com>
2021-04-04 21:55:21 -0500

    now 7 LEDs blinking in succession thrice

--- gpio-tests/blink-led/blink-led.py
+++ gpio-tests/blink-led/blink-led.py
@@ -5,21 +5,32 @@ import RPi.GPIO as GPIO
 import time
 
 # SET THESE TO THE DESIRED VALUES
-bcm_pin_num = 23
-sleep_time = 1
+bcm_pin_num1 = 21
+bcm_pin_num2 = 20
+bcm_pin_num3 = 26
+bcm_pin_num4 = 16
+bcm_pin_num5 = 19
+bcm_pin_num6 = 13
+bcm_pin_num7 = 12
+pins = [bcm_pin_num1, bcm_pin_num2, bcm_pin_num3, bcm_pin_num4, bcm_pin_num5, bcm_pin_num6, bcm_pin_num7]
+
+sleep_time = 0.5
 
 # configure the GPIO pins
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(bcm_pin_num, GPIO.OUT)
+for pin in pins:
+    GPIO.setup(pin, GPIO.OUT)
 
-# blink the LED on and off repeatedly
+# blink the LEDs on and off a few times
 try:
     for i in range(3):
-        GPIO.output(bcm_pin_num, GPIO.HIGH)
-        time.sleep(sleep_time)
-        GPIO.output(bcm_pin_num, GPIO.LOW)
-        time.sleep(sleep_time)
+        for pin in pins:
+            GPIO.output(pin, GPIO.HIGH)
+            time.sleep(sleep_time)
+            GPIO.output(pin, GPIO.LOW)
 finally:
-    # turn off LED and clean up pins when done
-    GPIO.output(bcm_pin_num, GPIO.LOW)
+    # turn off LEDs and clean up pins when done
+    for pin in pins:
+        GPIO.output(pin, GPIO.LOW)
+
     GPIO.cleanup()
